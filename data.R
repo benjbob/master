7b4cb7064e8d7141d19b810e6d13e56bddad6064
benjbob <benjamin.bayer1995@gmail.com>
2022-03-22 13:16:17 +0100

    new tickers

--- data.R
+++ data.R
@@ -1,18 +1,24 @@
 require(academictwitteR)
 require(tidyverse)
 
+read_csv("stoxx_names_ticker.csv", skip = 1)
+
+
 tweets <- get_all_tweets(
-             query = "$BUDFF",
-             start_tweets = "2015-01-01T00:00:00Z",
-             end_tweets = "2020-02-01T00:00:00Z",
-             exact_phrase = T,
+            query = "$BAYRY",
+            start_tweets = "2015-01-01T00:00:00Z",
+            end_tweets = "2020-02-01T00:00:00Z",
+            #exact_phrase = T,
             is_retweet = F,
             is_reply = F,
             is_quote = F,
-             n = 10
+            lang = c("en"),
+            has_cashtag = T,
+            n = 100
            )
 
 tweets$created_at
 tweets$text
-
+tweets$lang
 tweets %>% view()
+tweets$entities$cashtags
